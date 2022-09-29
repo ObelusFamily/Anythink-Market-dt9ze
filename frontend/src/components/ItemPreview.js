2e087a116245486bbab41ef6cb02ec4f932bccb3
Nathan Nonexistent <chupacabrot@users.noreply.github.com>
2022-09-29 20:00:56 +0000

    missing images

--- frontend/src/components/ItemPreview.js
+++ frontend/src/components/ItemPreview.js
@@ -37,7 +37,7 @@ const ItemPreview = (props) => {
       <img
         alt="item"
         src={item.image  ?  item.image  :  '/placeholder.png'}
-        className="card-img-top item-img c"
+        className="card-img-top item-img"
         style={{ borderRadius: "20px" }}
       />
       <div className="card-body">
